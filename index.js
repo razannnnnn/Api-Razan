49fd5b50bd4f7c6ebc17a0b47a9283e99e998883
razn-id <akuntest12346789@gmail.com>
2023-10-09 18:37:29 +0700

    update 404.ejs

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 app.use(function (err, req, res, next) {
-  res.send(loghandler.notfound);
+  res.render("404");
 });
 
 module.exports = app;
