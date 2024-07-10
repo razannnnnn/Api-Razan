4c2e0b340efb1827ab6de98fd7734e65f189d128
razn-id <akuntest12346789@gmail.com>
2024-07-10 15:27:26 +0700

    deploy to vercel

---
+++ public/js/home/script.js
@@ -0,0 +1,11 @@
+// main.js
+
+$(document).ready(function () {
+  // Activate Slick Slider
+  $(".testimonial-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: true,
+  });
+});
